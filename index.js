19d95696a05e422f0d7bccfa4ab27f630ecbb578
Marie Serradori <marie.serradori@gmail.com>
2022-03-17 10:12:40 +0100

    added chalk package and start script

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk"
+
+console.log(chalk.blue("Hello, Paul"))
+console.log(chalk.green("Hello, Zakari"))
+console.log(chalk.red("Hello, Lou"))
+console.log(chalk.black.bgYellow("Hope the triangle team is doing well !!"))
